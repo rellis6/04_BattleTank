d25ba1d2a5a5106f13a39af4fab7bb63dfbd0532
Robert <rellis6@gmail.com>
2017-03-19 21:07:17 -0400

    Lecture  117 - Using virtual and override: Added override for BeginPlay and logged out the tank name when starting the game.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,9 +3,18 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+	GetControlledTank();
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
+	if (GetPawn() != nullptr) {
+		FString pawnName = GetPawn()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Possed tank: %s"), *pawnName);
+	}
 	return Cast<ATank>(GetPawn());
 }
-
