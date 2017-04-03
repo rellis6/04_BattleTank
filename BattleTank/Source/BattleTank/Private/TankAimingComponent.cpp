1c24dd704f4775cd67ba4595d399cf24e4350b28
Robert <rellis6@gmail.com>
2017-04-03 18:50:30 -0400

    Lecture 127 - BlueprintCallable(): Added a C++ component that will actually do the aiming. Also added getting the barrel location of the tank from the blueprint itself.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -37,6 +37,12 @@ void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, 
 void UTankAimingComponent::AimAt(FVector HitLocation)
 {
 	auto OurTankName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at: %s"), *OurTankName, *HitLocation.ToString());
+	auto BarrelLocation = Barrel->GetComponentLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s aiming at: %s from %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation.ToString());
+}
+
+void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent *BarrelToSet)
+{
+	Barrel = BarrelToSet;
 }
 
