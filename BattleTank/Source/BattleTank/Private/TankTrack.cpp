a342f02196c71f11957512d1dc1e35c1c514b97e
Robert <rellis6@gmail.com>
2017-05-20 19:11:19 -0400

    Lecture 151 - ApplyForceAtLocation() in Action: Added force to the tracks controlls, force is added but still adjustments to be made

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,12 +4,19 @@
 #include "TankTrack.h"
 
 
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	//auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s: throttle!"), *Name);
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
 
-	//TODO Clamp actual throttle value so player can't over-drive
+	// TODO clamp actual throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -6,17 +6,20 @@
 #include "TankTrack.generated.h"
 
 /**
- * TankTrack is used to set maximum driving force, and to apply forces to the tank.
- */
+* TankTrack is used to set maximum driving force, and to apply forces to the tank.
+*/
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
-	
+
 public:
-	//Set value between -1 and +1
+	// Sets a throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
-	
+
+	//Max Force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; //Assume 40 ton tank and 1g acceleration
+
 };
